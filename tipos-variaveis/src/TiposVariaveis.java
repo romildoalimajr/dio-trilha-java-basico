629b45ecee87b9a1c6b895cc49108c4676be40cd
Romildo A. Lima Jr <odlimor.sevla@gmail.com>
2024-08-08 10:47:56 -0300

    estudo das variaveis constantes.

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,10 +1,19 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
         
+        String meuNome = "Romildo Jr.";
+        
         double salarioMinimo = 2500.33;
         short numeroCurto = 1;
         int numeroNormal = numeroCurto;
         short numeroCurto2 = (short) numeroNormal;
+
+        int numero = 1;
+        numero = 2;
+
+        System.out.println(numero);
+
+        final double VALOR_DE_PI = 3.14;
 
     }
 }
