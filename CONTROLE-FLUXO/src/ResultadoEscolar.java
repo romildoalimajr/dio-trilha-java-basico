301dfe99795f468acb8cc73a0f50b53394d3991f
Romildo A. Lima Jr <odlimor.sevla@gmail.com>
2024-08-15 11:05:35 -0300

    Implementação do Else If

--- CONTROLE-FLUXO/src/ResultadoEscolar.java
+++ CONTROLE-FLUXO/src/ResultadoEscolar.java
@@ -1,11 +1,14 @@
 public class ResultadoEscolar {
 
     public static void main(String[] args) {
-        int nota = 8;
+        int nota = 10;
 
         if(nota >= 7){
             System.out.println("\nAprovado");
-        }else{
+        }else if(nota >= 5 && nota < 7){
+            System.out.println("\nProva de Recuperação");
+        }
+        else{
             
             System.out.println("\nReprovado");
         }
