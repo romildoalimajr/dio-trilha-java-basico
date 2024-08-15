ab3c6e15bcd08e85d90d90d2032b0ebde191e604
Romildo A. Lima Jr <odlimor.sevla@gmail.com>
2024-08-15 10:10:04 -0300

    Aplicando o  Controle de Fluxo Composto.

--- CONTROLE-FLUXO/src/CaixaEletronico.java
+++ CONTROLE-FLUXO/src/CaixaEletronico.java
@@ -2,10 +2,13 @@ public class CaixaEletronico {
 
         public static void main(String[] args) {
             double saldo = 25.0;
-            double valorSolicitado = 25.0;
+            double valorSolicitado = 22.0;
 
             if(valorSolicitado < saldo){
                 saldo = saldo - valorSolicitado;
+                System.out.println("\n Novo saldo é R$ " + saldo);
+            }else{
+                System.out.println("\n Saldo insuficiente!!!!");
             }
 
             System.out.println("\nO saldo atual é R$ " + saldo);
---
+++ CONTROLE-FLUXO/src/ResultadoEscolar.java
@@ -0,0 +1,13 @@
+public class ResultadoEscolar {
+
+    public static void main(String[] args) {
+        int nota = 8;
+
+        if(nota >= 7){
+            System.out.println("\nAprovado");
+        }else{
+            
+            System.out.println("\nReprovado");
+        }
+    }    
+}
