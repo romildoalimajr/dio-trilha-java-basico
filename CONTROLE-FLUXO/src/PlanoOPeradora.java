8d9ce84f3cc2d1608560ac08e2069174c0d1bd4c
Romildo A. Lima Jr <odlimor.sevla@gmail.com>
2024-08-15 11:45:10 -0300

    Implementando o Switch Case.

---
+++ CONTROLE-FLUXO/src/PlanoOPeradora.java
@@ -0,0 +1,17 @@
+public class PlanoOPeradora {
+    public static void main(String[] args) {
+        String plano = "T";
+
+        switch (plano) {
+            case "T":{
+                System.out.println("100 minutos de ligação");
+            }
+            case "M":{
+                System.out.println("Whats e Instagram grátis");
+            }
+            case "B":{
+                System.out.println("100 minutos de ligação");
+            }
+        }
+    }
+}
