d267965560d4ce131a5ef9c63212bf475c8a7892
Romildo A. Lima Jr <odlimor.sevla@gmail.com>
2024-08-15 18:36:26 -0300

    Uso do For com Arrays.

---
+++ CONTROLE-FLUXO/src/ExemploForArrays.java
@@ -0,0 +1,9 @@
+public class ExemploForArrays {
+    public static void main(String[] args) {
+        String alunos[] = {"Romildo", "Alves", "de", "Lima", "Junior"};
+
+        for(int x=0; x < alunos.length; x++){
+            System.out.println("O aluno no indice x = " + x + " é " + alunos[x]);
+        }
+    }
+}
