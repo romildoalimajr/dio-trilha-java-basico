9d3f0bb83a0c05682627a4e757b34d146e6bc27d
Romildo A. Lima Jr <odlimor.sevla@gmail.com>
2024-08-15 11:13:54 -0300

    Implementando a Condição Ternária.

---
+++ CONTROLE-FLUXO/src/ResultadoEscolar02.java
@@ -0,0 +1,12 @@
+public class ResultadoEscolar02 {
+
+    public static void main(String[] args) {
+        int nota = 6;
+
+        //String resultado = nota >= 7 ? "Aprovado" : "Reprovado";
+        String resultado = nota >= 7 ? "Aprovado" : nota >= 5 && nota < 7 ? "Recuperação" : "Reprovado";
+
+        System.out.println("\n" + resultado);
+    }
+    
+}
